3e6f58c6da19ea3bce508771281e32b8b3cca901
Pawan Gupta <pawan.gupta@rsystems.com>
2018-12-18 14:27:14 +0530

    init comments

---
+++ src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { HeaderComponent } from './component/header/header.component';
+import { UserListComponent } from './component/user-list/user-list.component';
+import { VideoChatWindowComponent } from './component/video-chat-window/video-chat-window.component';
+import { TextChatWindowComponent } from './component/text-chat-window/text-chat-window.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ChatWindowComponent } from './component/chat-window/chat-window.component';
+import { SettingComponent } from './component/setting/setting.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule,MatSidenavModule ,MatButtonModule,
+  MatButtonToggleModule,MatIconModule,
+  
+   } from '@angular/material';
+
+@NgModule({
+  exports: [
+    MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  
+    MatNativeDateModule,
+    MatSidenavModule,
+    ]
+})
+export class MaterialModule {}
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    UserListComponent,
+    VideoChatWindowComponent,
+    TextChatWindowComponent,
+    ChatWindowComponent,
+    SettingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    FormsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
